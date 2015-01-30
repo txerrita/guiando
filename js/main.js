c68f9d352d1c00ce4a81e0c995efd28ee5cf8826
txerrita <joserra.o@gmail.com>
2015-01-30 02:36:42 +0100

    2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 'use strict';
 
+var applaunchCount = window.localStorage.getItem('launchCount');
+
+//Check if it already exists or not
+if(applaunchCount){
+   //This is a second time launch, and count = applaunchCount
+}else{
+  //Local storage is not set, hence first time launch. set the local storage item
+  window.localStorage.setItem('launchCount',1);
+  introJs().start();
+
+  //Do the other stuff related to first time launch
+};
 	    
 
 var onSuccess = function(position) {
